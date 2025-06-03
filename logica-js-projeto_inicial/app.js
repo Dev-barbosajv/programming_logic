8e5dcd6680b1331d16e5bcba27b0c250ba4cd302
Dev-barbosajs <joaovitorbarbosa513@gmail.com>
2025-06-02 21:06:10 -0300

    Vimos sobre if e else

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,6 +1,12 @@
 alert('Boas vindas ao jogo do numero secreto')
 
 let numeroSecreto = 29;
-prompt('Escolha um numero entre 1 e 30')
+let chute = prompt('Escolha um numero entre 1 e 30')
+
+if (chute == numeroSecreto) {
+    alert(`Isso voce acertou: ${numeroSecreto}`)
+} else {
+    alert("Não foi dessa vez")
+}
 
 
